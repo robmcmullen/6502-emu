37b499fb3381e26c115e0d775abfc48c889b15cb
Rob McMullen <feedback@playermissile.com>
2019-07-14 09:41:01 -0700

    Added #define guards so file can be included more than once

--- 6502.h
+++ 6502.h
@@ -1,3 +1,6 @@
+#ifndef _6502_EMU_H_
+#define _6502_EMU_H_
+
 #include <stdint.h>
 #include <stdbool.h>
 
@@ -85,3 +88,5 @@ int load_rom(char * filename, int load_addr);
 int step_cpu(int verbose);
 
 void save_memory(char * filename);
+
+#endif /* _6502_EMU_H_ */
